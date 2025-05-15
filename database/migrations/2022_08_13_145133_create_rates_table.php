60ec35295e8af16ecf2ca1ad45d294a50d93bc5b
mngitngit <melanie.ngitngit@yahoo.com>
2025-05-15 20:35:00 +0800

    dynamic saving or registrations

--- database/migrations/2022_08_13_145133_create_rates_table.php
+++ database/migrations/2022_08_13_145133_create_rates_table.php
@@ -14,6 +14,7 @@ class CreateRatesTable extends Migration
     public function up()
     {
         Schema::create('rates', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('category');
             $table->string('attending_option');
             $table->string('description');
