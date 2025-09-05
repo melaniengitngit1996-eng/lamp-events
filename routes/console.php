bf690849cc9ed8eb8de9816f72f8833d6495a061
mngitngit <melanie.ngitngit@yahoo.com>
2025-09-06 01:13:34 +0800

    middleware for allowing specific domains for embedding

--- routes/console.php
+++ routes/console.php
@@ -96,7 +96,7 @@ Artisan::command('cancel-bookings {event_id?}', function () {
                 ])->notify(new Registered($registration));
             }
 
-            $registration->updateBookingActivities($registration, $registration->booking_activities, array('Booking cancelled due to unsettled payment.'));
+            $registration->updateBookingActivities($registration, $registration->booking_activities, array('<b>System:</b> Booking cancelled due to unsettled payment.'));
 
             $this->comment('[' . $registration->uuid . '] ' . $registration->fullname . '\'s booking is now cancelled. Date Booked: ' . $registration->booked_date);
         }
