0a7d7a1c086de0f726e1e6c719a0950c7c8e7481
mngitngit <melanie.ngitngit@yahoo.com>
2025-10-20 13:06:59 +0800

    checkin closed

--- resources/views/checkin/closed.blade.php
+++ resources/views/checkin/closed.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="px-4 text-center mt-5">
 <img height="120" width="150" class="mt-5" style="margin-top: 8%;" src="/images/love.png" />
-<h2 class="font-monospace mt-5 w-auto">Thank you for joining us!</h2>
+<h1 class="font-monospace mt-5 w-auto">Thank you for joining us!</h1>
 <p class="mt-1">Be blessed physically, materially & spiritually.</p>
 </div>
 @endsection
