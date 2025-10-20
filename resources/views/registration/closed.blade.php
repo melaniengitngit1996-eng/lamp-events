e011a917ad231b41b4efc7397ea80f159a479be9
mngitngit <melanie.ngitngit@yahoo.com>
2025-10-20 13:06:10 +0800

    checkin closed

--- resources/views/registration/closed.blade.php
+++ resources/views/registration/closed.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="px-4 text-center">
 <img height="250" width="300" class="mt-5" style="margin-top: 8%;" src="/images/closed.png" />
-<h3 class="font-monospace mt-5 w-auto">Sorry, the registration is now closed.</h3>
+<h1 class="font-monospace mt-5 w-auto">Sorry, the registration is now closed.</h1>
 <p>For any registration issues or concerns, kindly reach out to your local Registrars.</p>
 </div>
 @endsection
