d913201cb84e07bfcaba08721dd9ecc6e30b565d
mngitngit <melanie.ngitngit@yahoo.com>
2025-12-27 13:34:05 +0800

    api fix

--- resources/views/checkin/show.blade.php
+++ resources/views/checkin/show.blade.php
@@ -7,7 +7,7 @@
             @if($_REQUEST['id'] == $all)
             <el-link type="primary" class="float-end" href="/{{$event->slug}}/check-in">Check In Another Delegate</el-link>
             @else
-            <el-link type="primary" class="float-end" href="/check-in/passes?id={{ $all }}">View All Passes</el-link>
+            <el-link type="primary" class="float-end" href="/{{$event->slug}}/check-in/passes?id={{ $all }}">View All Passes</el-link>
             @endif
         </div>
     </div>
