6cb1da31dc4cf9b3ba41d7f31fb8a539f15ab513
mngitngit <melanie.ngitngit@yahoo.com>
2026-08-25 19:59:25 +0800

    format

--- resources/js/components/SlotsTableComponent.vue
+++ resources/js/components/SlotsTableComponent.vue
@@ -1,66 +1,33 @@
 <template>
     <div>
-        <el-table
-        :data="tableData"
-        :span-method="objectSpanMethod"
-        size="mini"
-        border
-        style="width: 100%">
-            <el-table-column
-                prop="registration_type"
-                label="Registration Type"
-                align="center"
-                width="180">
+        <el-table :data="tableData" :span-method="objectSpanMethod" size="mini" border style="width: 100%">
+            <el-table-column prop="registration_type" label="Registration Type" align="center" width="180">
             </el-table-column>
-            <el-table-column
-                prop="description"
-                label="Description"
-                align="center"
-                width="180">
+            <el-table-column prop="description" label="Description" align="center" width="180">
             </el-table-column>
-            <el-table-column
-                prop="event_date"
-                label="Event Date"
-                align="center"
-                width="180">
+            <el-table-column prop="event_date" label="Event Date" align="center" width="180">
             </el-table-column>
-            <el-table-column
-                prop="seat_count"
-                label="Total Slots"
-                align="center"
-                width="130">
+            <el-table-column prop="seat_count" label="Total Slots" align="center" width="130">
             </el-table-column>
-            <el-table-column
-                prop="taken"
-                label="Taken"
-                align="center"
-                width="130">
+            <el-table-column prop="taken" label="Taken" align="center" width="130">
             </el-table-column>
-            <el-table-column
-                prop="available"
-                label="Remaining"
-                align="center"
-                width="130">
+            <el-table-column prop="available" label="Remaining" align="center" width="130">
             </el-table-column>
-            <el-table-column
-                prop="activities"
-                label="Activity">
+            <el-table-column prop="activities" label="Activity">
                 <template slot-scope="scope">
-                    <p class="m-0" style="font-size: x-small;" v-for="(activity, index) in scope.row.activities" :key="index">
-                        {{ activity.timestamp }} - {{activity.user}} - <i>{{activity.message}}</i>
+                    <p class="m-0" style="font-size: x-small;" v-for="(activity, index) in scope.row.activities"
+                        :key="index">
+                        {{ activity.timestamp }} - {{ activity.user }} - <i>{{ activity.message }}</i>
                     </p>
                 </template>
             </el-table-column>
             <el-table-column v-if="permissions.can_add_slots" align="center" width="120">
                 <template slot-scope="scope">
-                <el-button type="primary" size="mini" plain @click="openModal(scope.row)">Add Slot</el-button>
+                    <el-button type="primary" size="mini" plain @click="openModal(scope.row)">Add Slot</el-button>
                 </template>
             </el-table-column>
         </el-table>
-        <el-dialog
-            :title="dialogTitle"
-            :visible.sync="dialogVisible"
-            :width="$func.isMobileView() ? '95%' : '30%'">
+        <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" :width="$func.isMobileView() ? '95%' : '30%'">
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px">
                 <el-form-item label="Additional Slot Count" prop="number">
                     <el-input v-model="ruleForm.number"></el-input>
@@ -75,7 +42,7 @@
         </el-dialog>
     </div>
 </template>
-  
+
 <script>
 export default {
     props: {
@@ -109,9 +76,9 @@ export default {
     methods: {
         objectSpanMethod({ row, column, rowIndex, columnIndex }) {
             if (columnIndex === 0) {
-                if (rowIndex % (this.slots.length/2) === 0) {
+                if (rowIndex % (this.slots.length / 2) === 0) {
                     return {
-                        rowspan: (this.slots.length/2),
+                        rowspan: (this.slots.length / 2),
                         colspan: 1
                     };
                 } else {
@@ -135,25 +102,25 @@ export default {
                         additional_count: this.ruleForm.number,
                         notes: this.ruleForm.notes
                     })
-                    .then(async (response) => {
-                        this.dialogVisible = false;
-                        this.dialogTitle = null;
-                        this.selected = null;
-                        this.ruleForm = {
-                            number: null,
-                            notes: '',
-                        }
-                        
-                        this.$notify.success({
-                            title: 'Slot updated successfully!',
-                            message: 'Please refresh the page to see the changes.'
+                        .then(async (response) => {
+                            this.dialogVisible = false;
+                            this.dialogTitle = null;
+                            this.selected = null;
+                            this.ruleForm = {
+                                number: null,
+                                notes: '',
+                            }
+
+                            this.$notify.success({
+                                title: 'Slot updated successfully!',
+                                message: 'Please refresh the page to see the changes.'
+                            });
+                        })
+                        .catch(error => {
+                            this.$notify.error({
+                                title: error
+                            });
                         });
-                    })
-                    .catch(error => {
-                        this.$notify.error({
-                            title: error
-                        });
-                    });
                 } else {
                     return false;
                 }
